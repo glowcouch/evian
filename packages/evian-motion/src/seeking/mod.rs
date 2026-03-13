affc406a75b48903353728e3081584178f282735
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-03-14 10:28:53 +1300

    feat: enable boomerang controller

--- packages/evian-motion/src/seeking/mod.rs
+++ packages/evian-motion/src/seeking/mod.rs
@@ -70,30 +70,30 @@ where
         }
     }
 
-    // /// Moves the robot to a desired pose (position and heading).
-    // ///
-    // /// This motion uses a boomerang controller, which is a motion algorithm
-    // /// for moving differential drivetrains to a desired pose. Larger `lead`
-    // /// values will result in wider arcs, while smaller `lead` values will
-    // /// result in smaller arcs. You may need to tune the `lead` value in order
-    // /// to properly reach the desired heading by the end of the motion.
-    // pub fn boomerang<'a, M: Arcade, T: TracksPosition + TracksHeading + TracksVelocity>(
-    //     &mut self,
-    //     drivetrain: &'a mut Drivetrain<M, T>,
-    //     point: impl Into<Vec2<f64>>,
-    //     heading: Angle,
-    //     lead: f64,
-    // ) -> BoomerangFuture<'a, M, L, A, T> {
-    //     BoomerangFuture {
-    //         drivetrain,
-    //         target_heading: heading,
-    //         lead,
-    //         target_point: point.into(),
-    //         timeout: self.timeout,
-    //         tolerances: self.tolerances,
-    //         linear_controller: self.linear_controller.clone(),
-    //         angular_controller: self.angular_controller.clone(),
-    //         state: None,
-    //     }
-    // }
+    /// Moves the robot to a desired pose (position and heading).
+    ///
+    /// This motion uses a boomerang controller, which is a motion algorithm
+    /// for moving differential drivetrains to a desired pose. Larger `lead`
+    /// values will result in wider arcs, while smaller `lead` values will
+    /// result in smaller arcs. You may need to tune the `lead` value in order
+    /// to properly reach the desired heading by the end of the motion.
+    pub fn boomerang<'a, M: Arcade, T: TracksPosition + TracksHeading + TracksVelocity>(
+        &mut self,
+        drivetrain: &'a mut Drivetrain<M, T>,
+        point: impl Into<Vec2<f64>>,
+        heading: Angle,
+        lead: f64,
+    ) -> BoomerangFuture<'a, M, L, A, T> {
+        BoomerangFuture {
+            drivetrain,
+            target_heading: heading,
+            lead,
+            target_point: point.into(),
+            timeout: self.timeout,
+            tolerances: self.tolerances,
+            linear_controller: self.linear_controller.clone(),
+            angular_controller: self.angular_controller.clone(),
+            state: None,
+        }
+    }
 }
